42e00b3e15ad365ce43ef6c468d692c48d420fde
Repu <34271234+wanisramdani@users.noreply.github.com>
2019-10-04 18:26:42 +0100

    Update mangaBot.py

--- MangaBot/mangaBot.py
+++ MangaBot/mangaBot.py
@@ -1,11 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-"""
-But if you ask me, using the database is better:
-- You can monitor multiple releases/manga
-- You will gain more experience and familiarity with SQL
-- You can be like: *Tut*, I used Heroku and PostgreSQL and Telegram API and BeautifulSoup, how cool is that?
-"""
 
 
 def get_update(url):
